71d6fe927759b6cd9c2a82324766f9b9d3e3e8d9
Daniel García Paniagua <danielgarcia488.dg@gmail.com>
2020-11-27 20:02:45 -0600

    clases de la BD

---
+++ DB_Regiter_Login/BackEnd/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Regiter_Login.BackEnd
+{
+    class Empleado
+    {
+        public int idEmpleado { get; set; }
+        public String nombreCompleto { get; set; }
+        public String direccion { get; set; }
+        public int telefono { get; set; }
+        public String usuario { get; set; }
+        public String contrasenia { get; set; }
+        public String puesto { get; set; }
+    }
+}
---
+++ DB_Regiter_Login/BackEnd/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Regiter_Login.BackEnd
+{
+    class Venta
+    {
+        public int idVenta { get; set; }
+        public DateTime fecha { get; set; }
+        public int idEmpleado { get; set; }
+        public int idCliente { get; set; }
+    }
+}
