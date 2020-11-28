69442469a98f869b84ee895bafd71ece8e9eba36
Daniel García Paniagua <danielgarcia488.dg@gmail.com>
2020-11-27 21:25:37 -0600

    clases BD

---
+++ DB_Regiter_Login/BackEnd/DetalleVenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Regiter_Login.BackEnd
+{
+    class DetalleVenta
+    {
+        public int idVenta { get; set; }
+        public int idProducto { get; set; }
+        public int cantidad { get; set; }
+        public double precioXUnidad { get; set; }
+        public double descuento { get; set; }
+
+    }
+}
