6cd7aed1921335808f00418d326e5a42be815cd4
Daniel García Paniagua <danielgarcia488.dg@gmail.com>
2020-11-30 16:19:05 -0600

    modificaciones a vista empleado

--- DB_Regiter_Login/Program.cs
+++ DB_Regiter_Login/Program.cs
@@ -17,6 +17,7 @@ namespace DB_Regiter_Login
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
